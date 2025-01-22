7f83eca1c59960032212e042f01fd1dd7e2a0706
Ryan DeVault <ctrlvaultdel51@gmail.com>
2025-01-21 22:10:12 -0600

    Heavy modifications.

--- Assets/Scripts/GolfBallController.cs
+++ Assets/Scripts/GolfBallController.cs
@@ -6,8 +6,8 @@ public class GolfBallController : MonoBehaviour {
     [SerializeField] private LineRenderer lineRenderer;
 
     [Header("Attributes")]
-    [SerializeField] private float maxPower = 10.0f;
-    [SerializeField] private float power = 2.0f;
+    [SerializeField] private float maxPower = 10f;
+    [SerializeField] private float power = 2f;
     
 
     private bool isDragging;
@@ -23,16 +23,20 @@ public class GolfBallController : MonoBehaviour {
 
         // Left mouse button
         if (Input.GetMouseButtonDown(0) && distance <= 0.5f) DragStart();
-        if (Input.GetMouseButton(0) && isDragging) DragChange();
+        if (Input.GetMouseButton(0) && isDragging) DragChange(inputPosition);
         if (Input.GetMouseButtonUp(0) && isDragging) DragRelease(inputPosition);
     }
 
     private void DragStart(){
         isDragging = true;
+        lineRenderer.positionCount = 2;
     }
 
-    private void DragChange(){
-
+    private void DragChange(Vector2 inputPosition){
+        Vector2 direction = (Vector2)transform.position - inputPosition;
+        lineRenderer.SetPosition(0, transform.position);
+        // Reduce visual accuracy of line
+        lineRenderer.SetPosition(1, (Vector2)transform.position + Vector2.ClampMagnitude(direction * power / 2, maxPower / 2));
     }
 
     private void DragRelease(Vector2 inputPosition){
@@ -40,7 +44,7 @@ public class GolfBallController : MonoBehaviour {
         isDragging = false;
 
         // Didn't drag far enough
-        if (distance < 1.0f) return;
+        if (distance < 1f) return;
         
         Vector2 direction = (Vector2)transform.position - inputPosition;
         rb.linearVelocity = Vector2.ClampMagnitude(direction * power, maxPower);
